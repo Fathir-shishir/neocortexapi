d0c92457d8dfd0d04af37545ef3af2d9b666d89e
Fathir-shishir <mfa.shishir@gmail.com>
2024-06-20 15:45:30 +0200

    project name changed

--- source/MyCloudProjectSample/MyCloudProject/Program.cs
+++ source/MyCloudProjectSample/MyCloudProject/Program.cs
@@ -13,7 +13,7 @@ namespace MyCloudProject
         /// <summary>
         /// Your project ID from the last semester.
         /// </summary>
-        private static string projectName = "ML 22/23-6";
+        private static string projectName = "ML 23/24-8";
 
         string test;
 

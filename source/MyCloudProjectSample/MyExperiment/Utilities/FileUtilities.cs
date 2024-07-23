b55b63e69fdd2555e85c385e4a764a75bf40034c
AkashRoy5680 <royakash1272@gmail.com>
2024-07-23 05:30:00 +0200

    local storage file paths are now available

--- source/MyCloudProjectSample/MyExperiment/Utilities/FileUtilities.cs
+++ source/MyCloudProjectSample/MyExperiment/Utilities/FileUtilities.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,15 @@ namespace MyExperiment.Utilities
 {
     public class FileUtilities
     {
+        /// <summary>
+        /// Retreive localstorage file path location
+        /// </summary>
+        /// <param name="localfilePath"></param>
+        /// <returns></returns>
+        public static string GetLocalStorageFilePath(string localFilePath)
+        {
+            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                localFilePath);
+        }
     }
 }

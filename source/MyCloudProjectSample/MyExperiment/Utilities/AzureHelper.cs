937b0900c0be29f9216ed19dd281c6186661bc0f
Fathir-shishir <mfa.shishir@gmail.com>
2024-08-04 20:10:29 +0200

    declared Azure helper class

--- source/MyCloudProjectSample/MyExperiment/Utilities/AzureHelper.cs
+++ source/MyCloudProjectSample/MyExperiment/Utilities/AzureHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Azure.Storage.Blobs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace MyExperiment.Utilities
 {
-    internal class AzureHelper
+    public class AzureHelper
     {
+        public class AzureHelperClass
+        {
+            
+        }
     }
 }

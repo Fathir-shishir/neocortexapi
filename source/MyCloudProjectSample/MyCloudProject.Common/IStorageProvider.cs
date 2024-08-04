a01b55f433a2b67088e635dc03f7088f8450e93e
Fathir-shishir <mfa.shishir@gmail.com>
2024-08-04 19:37:25 +0200

    modified Storage provider

--- source/MyCloudProjectSample/MyCloudProject.Common/IStorageProvider.cs
+++ source/MyCloudProjectSample/MyCloudProject.Common/IStorageProvider.cs
@@ -11,28 +11,19 @@ namespace MyCloudProject.Common
     public interface IStorageProvider
     {
         /// <summary>
-        /// Downloads the input file for training. This file contains all required input for the experiment.
-        /// The file is stored in the cloud or any other kind of store or database.
+        /// Downloads the input file for training.
         /// </summary>
-        /// <param name="fileName">The name of the file at some remote (cloud) location from where the file will be downloaded.</param>
+        /// <param name="fileName">The name of the local file where the input is downloaded.</param>
         /// <returns>The fullpath name of the file as downloaded locally.</returns>
-        /// <remarks>See step 4 in the architecture picture.</remarks>
         Task<string> DownloadInputFile(string fileName);
 
-        /// <summary>
-        /// Uploadds the result of the experiment in the cloud or any other kind of store or database.
-        /// </summary>
-        /// <param name="fileName">The name of the file at some remote (cloud) location  where the file will be uploaded.</param>
-        /// <returns>Not used. It can be null.</returns>
-        /// <remarks>See step 4 (oposite way) in the architecture picture.</remarks>
-        Task<byte[]> UploadResultFile(string fileName, byte[] data);
+        Task UploadResultFile(string fileName, byte[] data);
 
         /// <summary>
-        /// Uploads results of the experiment to the remote (cloud) location. For example the fileshare or as the entity to the table storage.
+        /// Uploads results of the experiment as the entoty of the table storage.
         /// </summary>
         /// <param name="result"></param>
-        /// <returns>Not used.</returns>
-       /// <remarks>See step 5 in the architecture picture.</remarks>
+        /// <returns></returns>
         Task UploadExperimentResult(IExperimentResult result);
     }
 }

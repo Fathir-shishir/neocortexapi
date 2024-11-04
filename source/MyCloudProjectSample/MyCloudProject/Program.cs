a23a51424d58e77fb3994c91c1476566286bdeea
AkashRoy5680 <royakash1272@gmail.com>
2024-11-05 00:30:00 +0100

    error fixed for project name variable

--- source/MyCloudProjectSample/MyCloudProject/Program.cs
+++ source/MyCloudProjectSample/MyCloudProject/Program.cs
@@ -17,7 +17,7 @@ namespace MyCloudProject
         /// <summary>
         /// Your project ID from the last semester.
         /// </summary>
-        private static string _projectName = "ML 22/23-6";
+        private static string _projectName = "ML22/23-13 Investigate Influence of parameter MaxNewSynapseCount";
 
         string test;
 
